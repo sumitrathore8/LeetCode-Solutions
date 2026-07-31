2645ab260e1c9ab5b8ebdf532c21b06d70b4c6ae
sumitrathore8 <sumitrathore821889@gmail.com>
2026-07-31 19:20:18 +0530

    Time: 3 ms (35.53%), Space: 15.8 MB (95.13%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -3,80 +3,65 @@
 #include <string>
 using namespace std;
 
-class Stack{
-    public:
-    vector<int>item;
+class Stack {
+public:
+    vector<int> item;
     int top;
 
-    Stack(int m){
+    Stack(int m) {
         item.resize(m);
-        top=-1;
+        top = -1;
     }
 
-    void push(int x){
-        if(top==item.size()-1){
-            cout<<"satck overflow";
-            return ;
+    void push(int x) {
+        if (top == item.size() - 1) {
+            cout << "Stack Overflow";
+            return;
         }
-        top++;
-        item[top]=x;
+        item[++top] = x;
     }
 
-    int pop(){
-        if(top==-1){
-            cout<<"stack overflow";
-            return '\0';
+    int pop() {
+        if (top == -1) {
+            cout << "Stack Underflow";
+            return -1;
         }
-        int x=item[top];
-        top--;
-        return x;
+        return item[top--];
     }
 
-    int Stacktop(){
-        int x=item[top];
-        return x;
-    }
-
-    bool Isempty(){
-        if(top==-1){
-        return true;
+    int Stacktop() {
+        if (top == -1) {
+            return -1;
         }
-        else{
-        return false;
-        }
+        return item[top];
     }
 
+    bool Isempty() {
+        return top == -1;
+    }
 };
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        Stack s(1000000);
-        for(int i=0;i<tokens.size();i++){
-            
-            if(tokens[i]!="+" && tokens[i]!="/" && tokens[i]!="*" && tokens[i]!="-" ){
-                int n=stoi(tokens[i]);
-                s.push(n);
+    Stack s(tokens.size());
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/") {
+                s.push(stoi(tokens[i]));
             }
-            else{
-                int b=s.Stacktop();
-                s.pop();
-                int a=s.Stacktop();
-                s.pop();
-                if(tokens[i]=="+"){
-                    s.push(a+b);
-                }
-                else if(tokens[i]=="/"){
-                    s.push(a/b);
-                }
-                else if(tokens[i]=="-"){
-                    s.push(a-b);
-                }
-                else{
-                    s.push(a*b);
-                }    
+            else {
+                int b = s.pop();
+                int a = s.pop();
+                if (tokens[i] == "+")
+                    s.push(a + b);
+                else if (tokens[i] == "-")
+                    s.push(a - b);
+                else if (tokens[i] == "*")
+                    s.push(a * b);
+               else
+                    s.push(a / b);
             }
         }
-        int ans=s.Stacktop();
-        return ans;
+
+        return s.Stacktop();
     }
 };
